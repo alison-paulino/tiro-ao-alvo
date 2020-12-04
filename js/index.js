83d5938d98592372bf7c7792d79c90108e9367b5
alison <alisonadriel@hotmail.com>
2020-12-03 22:38:54 -0300

    classe ball com check crash

--- js/index.js
+++ js/index.js
@@ -10,26 +10,29 @@ window.onload = function() {
       this.ctx = this.canvas.getContext('2d');
       this.canvas.width = 800;
       this.canvas.height = 500;
-      this.interval = setInterval(updateGameArea, 10);
+      this.interval = setInterval(updateGameArea, 20);
     },
     clear: function () {
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
+    }, 
+    balls : []
   }
   function startGame(){
     myGameArea.start();
     background.draw();
     snowMan.draw(); 
     snowBall.draw();
+    
   }
   function updateGameArea(){
-    console.log('entrei no update')
     myGameArea.clear();
     background.draw();
     snowMan.draw();
     snowMan.move();
     //snowMan.checkCrash();
     snowBall.draw();
+    snowBall.move();
+    snowBall.directionControl();
   }
   class Background {
     constructor(source, width, height){
@@ -41,7 +44,6 @@ window.onload = function() {
       this.height = height;
     }
     draw() {
-    console.log('desenhei o background')
     myGameArea.ctx.drawImage(this.img, this.x, this.y,this.width, this.height);
     }
   }  
@@ -57,8 +59,41 @@ window.onload = function() {
       this.speed = 0;
     }
     draw(){
-      console.log("desenhando a bola!");
     myGameArea.ctx.drawImage(this.img, this.x, this.y,this.width, this.height);    
+    }
+    move(){
+      console.log(this.y)
+      
+      if(this.y < 440 && this.y > 305) {
+        this.speed = -1;
+      this.y += this.speed;
+      this.width -= 0.5;
+      this.height -= 0.5;
+      this.x -= 0.1;
+      }
+      if(this.y === 305){
+        console.log("check crash")
+        
+        if(snowMan.checkCrash(myGameArea.balls[0])===true){
+        myGameArea.ctx.fillRect(300,300,100,100);
+        myGameArea.ctx.clearRect(300,300,100,100);
+        console.log(`x da bola ${this.x}`);
+        console.log(`x do boneco ${snowMan.x}`)
+        }
+      }
+     }
+  left(){
+      return this.x;
+    }
+    rigth(){
+      return this.x + this.width;
+    }
+    directionControl(){
+      myGameArea.ctx.beginPath();
+      myGameArea.ctx.fillRect(0, 480, 800,20);
+      myGameArea.ctx.fillStyle = 'blue';
+      myGameArea.ctx.fill();
+      myGameArea.ctx.closePath()
     }
   }
 
@@ -70,18 +105,15 @@ window.onload = function() {
       this.y = 160;
       this.width = width;
       this.height = height;
-      this.speed = 1;
+      this.speed = 2;
     }
     draw(){
-      console.log('desenhei o boneco')
         myGameArea.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
     move(){
      this.x += this.speed;
-     if(this.x === 620) {
-       this.speed = -1;
-     }
-     if(this.x === 120) this.speed = 1
+     if(this.x === 620) this.speed = -2;
+     if(this.x === 120) this.speed = 2
     }
     left(){
       return this.x;
@@ -96,7 +128,10 @@ window.onload = function() {
 
   
   const snowMan = new SnowMan('../images/boneco3.png',120, 120, 200);
-  const snowBall = new SnowBall('../images/bola-de-neve.png',300, 400, 100, 100);
+  const snowBall = new SnowBall('../images/bola-de-neve.png',450, 439, 100, 100);
+  myGameArea.balls.push(snowBall);
+  console.log(myGameArea.balls);
+
   const background = new Background('../images/fundo6.jpg',800, 500);
   
 }  
