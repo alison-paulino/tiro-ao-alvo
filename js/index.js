2466ddc74e8c3d400cbf6b3769b3130186bf6e90
alison <alisonadriel@hotmail.com>
2020-12-05 18:37:42 -0300

    force e direction control implemeted

--- js/index.js
+++ js/index.js
@@ -5,8 +5,10 @@ window.onload = function() {
  
  const myGameArea = {
     canvas: document.querySelector('#canvas'),
+    clicks : 0,
+    press : 0,
       start: function() {
-        console.log('entrei na start')
+      
       this.ctx = this.canvas.getContext('2d');
       this.canvas.width = 800;
       this.canvas.height = 500;
@@ -16,6 +18,7 @@ window.onload = function() {
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }, 
     balls : [],
+  
     /*stop : function (){
       this.clear();
       background.draw();
@@ -25,7 +28,7 @@ window.onload = function() {
     myGameArea.start();
     background.draw();
     snowMan.draw(); 
-    snowBall.draw();
+    //snowBall.draw();
   }
   function updateGameArea(){
     myGameArea.clear();
@@ -33,10 +36,15 @@ window.onload = function() {
     snowMan.draw();
     snowMan.move();
     //snowMan.checkCrash();
-    snowBall.draw();
-    snowBall.move();
-    snowBall.directionControl();
+    directionControl.draw();
+    directionControl.move();
+    forceControl.draw();
+    forceControl.move();
+    myGameArea.balls[0].draw();
+    myGameArea.balls[0].move();
+    
   }
+  
   class Background {
     constructor(source, width, height){
       this.img = new Image();
@@ -52,38 +60,65 @@ window.onload = function() {
   }  
 
   class SnowBall{
-    constructor(source, x, y, width, height){
+    constructor(source, x, y, width, height,speed){
       this.img = new Image();
       this.img.src = source;
       this.x = x;
       this.y = y;
       this.width = width;
       this.height = height;
-      this.speed = 0;
+      this.speed = speed;
     }
     draw(){
     myGameArea.ctx.drawImage(this.img, this.x, this.y,this.width, this.height);    
     }
-    move(){
-      console.log(this.y)
-      
+    move(){ 
+      if(this.speed >= 150 && this.speed < 187) this.speed = -0.8
+      if(this.speed >= 187 && this.speed < 224) this.speed = -0.9
+      if(this.speed >= 224 && this.speed < 261) this.speed = -1
+      if(this.speed >= 261 && this.speed < 298) this.speed = -1.1
+      if(this.speed >= 298 && this.speed < 330) this.speed = -1.2
+
+
       if(this.y < 440 && this.y > 305) {
-        this.speed = -1;
-      this.y += this.speed;
-      this.width -= 0.5;
-      this.height -= 0.5;
-      this.x -= 0.1;
+      if(this.speed == -0.8){ 
+        this.y += this.speed;
+        this.width -= 0.45;
+        this.height -= 0.45;
+        //this.x -= 0.1;
       }
-      if(this.y === 305){
-        console.log("check crash")
-        
+      if(this.speed == -0.9){ 
+        this.y += this.speed;
+        this.width -= 0.46;
+        this.height -= 0.46;
+        //this.x -= 0.1;
+      }
+      if(this.speed == -1){ 
+        this.y += this.speed;
+        this.width -= 0.5;
+        this.height -= 0.5;
+        //this.x -= 0.1;
+      }
+      if(this.speed == -1.1){ 
+        this.y += this.speed;
+        this.width -= 0.6;
+        this.height -= 0.6;
+        //this.x -= 0.1;
+      }
+      if(this.speed == -1.2){ 
+        this.y += this.speed;
+        this.width -= 0.65;
+        this.height -= 0.65;
+        //this.x -= 0.1;
+      }
+      
+      }
+      if(this.y <= 305){
         if(snowMan.checkCrash(myGameArea.balls[0])===true){
         myGameArea.ctx.fillRect(300,300,100,100);
         myGameArea.ctx.clearRect(300,300,100,100);
-        console.log(`x da bola ${this.x}`);
-        console.log(`x do boneco ${snowMan.x}`)
         }
-        myGameArea.stop();
+       // myGameArea.stop();
       }
      }
   left(){
@@ -91,13 +126,6 @@ window.onload = function() {
     }
     rigth(){
       return this.x + this.width;
-    }
-    directionControl(){
-      myGameArea.ctx.beginPath();
-      myGameArea.ctx.fillRect(0, 480, 800,20);
-      myGameArea.ctx.fillStyle = 'blue';
-      myGameArea.ctx.fill();
-      myGameArea.ctx.closePath()
     }
   }
 
@@ -120,10 +148,10 @@ window.onload = function() {
      if(this.x === 120) this.speed = 2
     }
     left(){
-      return this.x;
+      return this.x + 15;
     }
     rigth(){
-      return this.x + this.width;
+      return this.x -15 + this.width;
     }
     checkCrash(ball){
       return !(ball.left()> this.rigth()||ball.rigth()<this.left())
@@ -132,10 +160,99 @@ window.onload = function() {
 
   
   const snowMan = new SnowMan('../images/boneco3.png',120, 120, 200);
-  const snowBall = new SnowBall('../images/bola-de-neve.png',450, 439, 100, 100);
-  myGameArea.balls.push(snowBall);
-  console.log(myGameArea.balls);
-
-  const background = new Background('../images/fundo6.jpg',800, 500);
+  //const snowBall = new SnowBall('../images/bola-de-neve.png',450, 439, 100, 100);
+  //snowBall = new SnowBall();
+  function newBall(x,speed){
+    myGameArea.balls.push(new SnowBall('./images/bola-de-neve.png',x, 439, 100, 100, speed));
+    console.log(myGameArea.balls);
+  }
   
+/*  
+document.addEventListener('click', getPosition, true);
+let clickPosition = {x : 0, speed : 0}
+function getPosition(e) {
+  if(myGameArea.clicks === 0){
+    // click botao start
+    console.log(myGameArea.clicks);
+    }
+    if(myGameArea.clicks ===1){
+      clickPosition.x = e.pageX;
+      console.log(myGameArea.clicks);
+    }
+    if(myGameArea.clicks ===2){
+      clickPosition.speed = e.pageY;
+      console.log(myGameArea.clicks);
+      newBall(clickPosition.x, clickPosition.speed);
+      myGameArea.clicks = 0;
+    }
+    myGameArea.clicks += 1;    
+} */
+let getX = 0;
+let getSpeed = 0;
+document.addEventListener('keydown', (e) => {
+  console.log(myGameArea.press);
+  if(myGameArea.press === 0){
+    if (e.keyCode === 32) {
+      getX = directionControl.x;
+      directionControl.z = 0;
+      console.log(getX);
+    }
+  }
+  if(myGameArea.press === 1){
+    if (e.keyCode === 32) {
+      getSpeed = forceControl.speed;
+      forceControl.z = 0;
+      console.log(getSpeed);
+      newBall(getX, getSpeed);
+      myGameArea.press = 1;
+      
+    }
+  }
+  myGameArea.press +=1;
+})
+
+class ForceControl{
+  constructor(){
+    this.speed = 150;
+    this.z = 0;
+  }
+  draw(){
+    myGameArea.ctx.fillStyle = 'red';
+    myGameArea.ctx.strokeRect(10, 150, 20,200);
+    myGameArea.ctx.fill();
+    myGameArea.ctx.beginPath(); 
+    myGameArea.ctx.fillRect(10, this.speed, 20, 20);
+    myGameArea.ctx.closePath();
+  }
+  move(){
+    this.speed += this.z;
+    if(this.speed === 150) this.z = 2;
+   if(this.speed === 330)this.z = -2
+  }
+}  
+
+ class DirectionControl{
+   constructor(){
+     this.x = 0;
+     this.z = 0;
+   }
+ 
+  draw(){
+    myGameArea.ctx.fillStyle = 'red';
+    myGameArea.ctx.strokeRect(0, 480, 800,20);
+    myGameArea.ctx.fill();
+    myGameArea.ctx.beginPath(); 
+    myGameArea.ctx.arc(this.x, 480, 15, 0, Math.PI *2);
+    //myGameArea.ctx.fillRect((this.x), 480, 800,15);
+    myGameArea.ctx.closePath();
+  }
+  move(){
+    this.x += this.z;
+     if(this.x === 0) this.z = 2;
+    if(this.x === 800)this.z = -2
+  }
+}
+  const background = new Background('../images/fundo6.jpg',800, 500);
+  const directionControl = new DirectionControl();
+  const forceControl = new ForceControl();
 }  
