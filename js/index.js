539ba9beb87a4465923507c6134778f929716737
alison <alisonadriel@hotmail.com>
2020-12-08 23:52:40 -0300

    verificando nova jogada

--- js/index.js
+++ js/index.js
@@ -2,13 +2,14 @@ window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
   }
+  
  
  const myGameArea = {
     canvas: document.querySelector('#canvas'),
     clicks : 0,
     press : 0,
+    balls : [],
       start: function() {
-      
       this.ctx = this.canvas.getContext('2d');
       this.canvas.width = 800;
       this.canvas.height = 500;
@@ -17,12 +18,16 @@ window.onload = function() {
     clear: function () {
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }, 
-    balls : [],
-  
-    /*stop : function (){
-      this.clear();
-      background.draw();
-    }*/
+    stop : function (){
+      console.log('entrei no stop')
+      clearInterval(this.interval);
+      console.log('executei clear interval')
+      setTimeout(this.gameOver, 1000);
+    },
+    gameOver: function () {
+      console.log('entrei no game over')
+      myGameArea.clear();
+    }
   }
   function startGame(){
     myGameArea.start();
@@ -42,7 +47,7 @@ window.onload = function() {
     forceControl.move();
     myGameArea.balls[0].draw();
     myGameArea.balls[0].move();
-    
+    verColisao();
   }
   
   class Background {
@@ -73,59 +78,73 @@ window.onload = function() {
     myGameArea.ctx.drawImage(this.img, this.x, this.y,this.width, this.height);    
     }
     move(){ 
-      if(this.speed >= 150 && this.speed < 187) this.speed = -0.8
-      if(this.speed >= 187 && this.speed < 224) this.speed = -0.9
-      if(this.speed >= 224 && this.speed < 261) this.speed = -1
-      if(this.speed >= 261 && this.speed < 298) this.speed = -1.1
-      if(this.speed >= 298 && this.speed < 330) this.speed = -1.2
-
-
-      if(this.y < 440 && this.y > 305) {
-      if(this.speed == -0.8){ 
+      if(this.speed >= 150 && this.speed < 187) this.speed = -2.5
+      if(this.speed >= 187 && this.speed < 224) this.speed = -2
+      if(this.speed >= 224 && this.speed < 261) this.speed = -1.5
+      if(this.speed >= 261 && this.speed < 298) this.speed = -1
+      if(this.speed >= 298 && this.speed < 330) this.speed = -0.5
+      if(this.y < 440 && this.y >= 305) {
+      if(this.speed == -0.5){ 
         this.y += this.speed;
-        this.width -= 0.45;
-        this.height -= 0.45;
-        //this.x -= 0.1;
-      }
-      if(this.speed == -0.9){ 
-        this.y += this.speed;
-        this.width -= 0.46;
-        this.height -= 0.46;
-        //this.x -= 0.1;
+        this.width -= 0.25;
+        this.height -= 0.25;
+        if(this.x > 500) this.x -= 0.1;
       }
       if(this.speed == -1){ 
         this.y += this.speed;
         this.width -= 0.5;
         this.height -= 0.5;
-        //this.x -= 0.1;
+        if(this.x > 500) this.x -= 0.1;
       }
-      if(this.speed == -1.1){ 
+      if(this.speed == -1.5){ 
         this.y += this.speed;
-        this.width -= 0.6;
-        this.height -= 0.6;
-        //this.x -= 0.1;
+        this.width -= 0.75;
+        this.height -= 0.75;
+        if(this.x > 500) this.x -= 0.1;
       }
-      if(this.speed == -1.2){ 
+      if(this.speed == -2){ 
         this.y += this.speed;
-        this.width -= 0.65;
-        this.height -= 0.65;
-        //this.x -= 0.1;
+        this.width -= 1;
+        this.height -= 1;
+        if(this.x > 500) this.x -= 0.1;
+      }
+      if(this.speed == -2.5){ 
+        this.y += this.speed;
+        this.width -= 1.3;
+        this.height -= 1.3;
+        if(this.x > 500) this.x -= 0.1;
       }
       
       }
-      if(this.y <= 305){
-        if(snowMan.checkCrash(myGameArea.balls[0])===true){
-        myGameArea.ctx.fillRect(300,300,100,100);
-        myGameArea.ctx.clearRect(300,300,100,100);
-        }
-       // myGameArea.stop();
-      }
+      
      }
   left(){
       return this.x;
     }
     rigth(){
       return this.x + this.width;
+    }
+  }
+
+  function verColisao(){
+    if(myGameArea.balls[0].y < 305){
+        
+      if(snowMan.checkCrash(myGameArea.balls[0])===true){
+        console.log('colisão')
+        myGameArea.balls[0].speed = 0;
+        myGameArea.balls[0].y = 306;
+
+        myGameArea.stop();
+      //myGameArea.ctx.fillRect(300,300,100,100);
+      //myGameArea.ctx.clearRect(300,300,100,100);
+      
+      }
+      if(snowMan.checkCrash(myGameArea.balls[0])===false){
+        console.log('não colidiu')
+      myGameArea.ctx.fillRect(400,400,100,100);
+      myGameArea.ctx.clearRect(400,400,100,100);
+      }
+     
     }
   }
 
@@ -146,6 +165,7 @@ window.onload = function() {
      this.x += this.speed;
      if(this.x === 620) this.speed = -2;
      if(this.x === 120) this.speed = 2
+    
     }
     left(){
       return this.x + 15;
@@ -161,7 +181,7 @@ window.onload = function() {
   
   const snowMan = new SnowMan('../images/boneco3.png',120, 120, 200);
   //const snowBall = new SnowBall('../images/bola-de-neve.png',450, 439, 100, 100);
-  //snowBall = new SnowBall();
+  const snowBall = new SnowBall();
   function newBall(x,speed){
     myGameArea.balls.push(new SnowBall('./images/bola-de-neve.png',x, 439, 100, 100, speed));
     console.log(myGameArea.balls);
