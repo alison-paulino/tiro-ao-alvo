25dce4021d427f7c644dc07b1115ba805316f5a1
alison <alisonadriel@hotmail.com>
2020-12-14 18:58:56 -0300

    controle de volume

--- js/sons.js
+++ js/sons.js
@@ -3,3 +3,9 @@ const music = new Array();
 music[0] = new Audio('./sound/crash.wav');
 music[1] = new Audio('./sound/music-game.mp3');
 music[2] = new Audio('./sound/game-over.wav');
+
+music[0].volume = 0.25;
+music[1].volume = 0.25;
+music[2].volume = 0.25;
+
+music[1].loop = true;
