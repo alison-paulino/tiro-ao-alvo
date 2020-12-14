5e4ae4be9585d26a7ed6c29b91c7f429169a8fd3
alison <alisonadriel@hotmail.com>
2020-12-14 18:49:12 -0300

    incluindo som

--- js/sons.js
+++ js/sons.js
@@ -1,5 +1,5 @@
 
 const music = new Array();
-music[0] = new Audio('../sound/crash.wav');
-music[1] = new Audio('../sound/music-game.mp3');
-music[2] = new Audio('../sound/game-over.wav');
+music[0] = new Audio('./sound/crash.wav');
+music[1] = new Audio('./sound/music-game.mp3');
+music[2] = new Audio('./sound/game-over.wav');
